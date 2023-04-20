08f7d26a6696b149cfd58063982baef7cb7753ff
shovon <mohammad.moniruzzaman@brainstation-23.com>
2023-04-20 16:00:28 +0600

    hrms customization starts

--- resources/views/job/joblist.blade.php
+++ resources/views/job/joblist.blade.php
@@ -30,7 +30,7 @@
                     </div>
                 </li>
                 <!-- /Search -->
-            
+
                 <!-- Flag -->
                 <li class="nav-item dropdown has-arrow flag-nav">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button">
@@ -41,7 +41,7 @@
                             <img src="{{ URL::to('assets/img/flags/us.png') }}" alt="" height="16"> English
                         </a>
                         <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="{{ URL::to('assets/img/flags/kh.png') }}" alt="" height="16"> Khmer 
+                            <img src="{{ URL::to('assets/img/flags/kh.png') }}" alt="" height="16"> Khmer
                         </a>
                         <a href="javascript:void(0);" class="dropdown-item">
                             <img src="{{ URL::to('assets/img/flags/fr.png') }}" alt="" height="16"> French
@@ -55,7 +55,7 @@
                     </div>
                 </li>
                 <!-- /Flag -->
-                
+
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </li>
@@ -93,146 +93,6 @@
                 </div>
                 <!-- /Page Header -->
                 <div class="row">
-                    <div class="col-md-6">
-                        <a class="job-list" href="{{ route('form/job/view') }}">
-                            <div class="job-list-det">
-                                <div class="job-list-desc">
-                                    <h3 class="job-list-title">Web Devloper</h3>
-                                    <h4 class="job-department">Development</h4>
-                                </div>
-                                <div class="job-type-info">
-                                    <span class="job-types">Full Time</span>
-                                </div>
-                            </div>
-                            <div class="job-list-footer">
-                                <ul>
-                                    <li><i class="fa fa-map-signs"></i> California</li>
-                                    <li><i class="fa fa-money"></i> $35000-$38000</li>
-                                    <li><i class="fa fa-clock-o"></i> 2 days ago</li>
-                                </ul>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <a class="job-list" href="{{ route('form/job/view') }}">
-                            <div class="job-list-det">
-                                <div class="job-list-desc">
-                                    <h3 class="job-list-title">Android Devloper</h3>
-                                    <h4 class="job-department">App Development</h4>
-                                </div>
-                                <div class="job-type-info">
-                                    <span class="job-types">Part Time</span>
-                                </div>
-                            </div>
-                            <div class="job-list-footer">
-                                <ul>
-                                    <li><i class="fa fa-map-signs"></i> California</li>
-                                    <li><i class="fa fa-money"></i> $35000-$38000</li>
-                                    <li><i class="fa fa-clock-o"></i> 2 days ago</li>
-                                </ul>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <a class="job-list" href="{{ route('form/job/view') }}">
-                            <div class="job-list-det">
-                                <div class="job-list-desc">
-                                    <h3 class="job-list-title">Web Devloper</h3>
-                                    <h4 class="job-department">Development</h4>
-                                </div>
-                                <div class="job-type-info">
-                                    <span class="job-types">Full Time</span>
-                                </div>
-                            </div>
-                            <div class="job-list-footer">
-                                <ul>
-                                    <li><i class="fa fa-map-signs"></i> California</li>
-                                    <li><i class="fa fa-money"></i> $35000-$38000</li>
-                                    <li><i class="fa fa-clock-o"></i> 2 days ago</li>
-                                </ul>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <a class="job-list" href="{{ route('form/job/view') }}">
-                            <div class="job-list-det">
-                                <div class="job-list-desc">
-                                    <h3 class="job-list-title">Android Devloper</h3>
-                                    <h4 class="job-department">App Development</h4>
-                                </div>
-                                <div class="job-type-info">
-                                    <span class="job-types">Part Time</span>
-                                </div>
-                            </div>
-                            <div class="job-list-footer">
-                                <ul>
-                                    <li><i class="fa fa-map-signs"></i> California</li>
-                                    <li><i class="fa fa-money"></i> $35000-$38000</li>
-                                    <li><i class="fa fa-clock-o"></i> 2 days ago</li>
-                                </ul>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <a class="job-list" href="{{ route('form/job/view') }}">
-                            <div class="job-list-det">
-                                <div class="job-list-desc">
-                                    <h3 class="job-list-title">Web Devloper</h3>
-                                    <h4 class="job-department">Development</h4>
-                                </div>
-                                <div class="job-type-info">
-                                    <span class="job-types">Full Time</span>
-                                </div>
-                            </div>
-                            <div class="job-list-footer">
-                                <ul>
-                                    <li><i class="fa fa-map-signs"></i> California</li>
-                                    <li><i class="fa fa-money"></i> $35000-$38000</li>
-                                    <li><i class="fa fa-clock-o"></i> 2 days ago</li>
-                                </ul>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <a class="job-list" href="{{ route('form/job/view') }}">
-                            <div class="job-list-det">
-                                <div class="job-list-desc">
-                                    <h3 class="job-list-title">Android Devloper</h3>
-                                    <h4 class="job-department">App Development</h4>
-                                </div>
-                                <div class="job-type-info">
-                                    <span class="job-types">Part Time</span>
-                                </div>
-                            </div>
-                            <div class="job-list-footer">
-                                <ul>
-                                    <li><i class="fa fa-map-signs"></i> California</li>
-                                    <li><i class="fa fa-money"></i> $35000-$38000</li>
-                                    <li><i class="fa fa-clock-o"></i> 2 days ago</li>
-                                </ul>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6">
-                        <a class="job-list" href="{{ route('form/job/view') }}">
-                            <div class="job-list-det">
-                                <div class="job-list-desc">
-                                    <h3 class="job-list-title">Web Devloper</h3>
-                                    <h4 class="job-department">Development</h4>
-                                </div>
-                                <div class="job-type-info">
-                                    <span class="job-types">Full Time</span>
-                                </div>
-                            </div>
-                            <div class="job-list-footer">
-                                <ul>
-                                    <li><i class="fa fa-map-signs"></i> California</li>
-                                    <li><i class="fa fa-money"></i> $35000-$38000</li>
-                                    <li><i class="fa fa-clock-o"></i> 2 days ago</li>
-                                </ul>
-                            </div>
-                        </a>
-                    </div>
                     <div class="col-md-6">
                         <a class="job-list" href="{{ route('form/job/view') }}">
                             <div class="job-list-det">
